f174a1c4bc67d0e043bba724c8c8ee54da46fe71
Cheese <yolosolodbaz@gmail.com>
2021-08-13 19:19:54 -0600

    untabification

--- JET/Patches/Core/DisplayMoneyAmountPatch.cs
+++ JET/Patches/Core/DisplayMoneyAmountPatch.cs
@@ -10,95 +10,95 @@ using System.Threading.Tasks;
 namespace JET.Patches
 {
     class DisplayMoneyAmountPatch : GenericPatch<DisplayMoneyAmountPatch>
-	{
-		private string _MethodName = "Show";
-		private string _FieldName = "Succeed";
+    {
+        private string _MethodName = "Show";
+        private string _FieldName = "Succeed";
 
-		public DisplayMoneyAmountPatch() : base(prefix: nameof(PatchPrefix)) { }
+        public DisplayMoneyAmountPatch() : base(prefix: nameof(PatchPrefix)) { }
 
-		protected override MethodBase GetTargetMethod()
-		{
-			return typeof(EFT.UI.DisplayMoneyPanel).GetMethod(_MethodName, BindingFlags.Public | BindingFlags.Instance);
-		}
-		/* need to be patched using transpiler and edited a part where GetMoneySums is called then returned the one below ;)
-		EFT.ItemRequirementsPanel.CreateItemsViews(GClass1560, GClass1928[], Action<bool>) : void @06007242
-		EFT.UI.HealthTreatmentServiceView.method_3() : void @0600ACC1
-		EFT.UI.Insurance.InsurerParametersPanel.UpdateLabels(int, bool, bool) : void @0600BCFA
-		EFT.UI.QuestObjectiveView.method_0(GClass1448) : void @0600AB9D
-		EFT.UI.Ragfair.AddOfferWindow.method_3() : void @0600C030
-		EFT.UI.Ragfair.RenewOfferWindow.method_2() : void @0600C17E
-		EFT.UI.RepairerParametersPanel.UpdateLabels() : void @0600A4BC
-		GClass1460.method_16(GClass1781, ConditionFindItem) : int @06007FF2
-		*/
-		private static bool PatchPrefix(
-			IEnumerable<EFT.InventoryLogic.Item> inventoryItems,
-			EFT.UI.DisplayMoneyPanel __instance,
-			CustomTextMeshProUGUI ___roubles,
-			CustomTextMeshProUGUI ___euros,
-			CustomTextMeshProUGUI ___dollars)
-		{
-			__instance.ShowGameObject(false);
-			var items = inventoryItems.SelectMany(new Func<Item, IEnumerable<Item>>(GetAllItems));
-			var OnlyMoney = items.Where(new Func<EFT.InventoryLogic.Item, bool>(IsMoney));
-			long CNT_ROUBLES = 0;
-			long CNT_DOLLARS = 0;
-			long CNT_EUROS = 0;
-			foreach (var money in OnlyMoney) {
-				switch (money.TemplateId) {
-					case "5449016a4bdc2d6f028b456f": // Roubles
-						CNT_ROUBLES += money.StackObjectsCount;
-						break;
-					case "5696686a4bdc2da3298b456a": // Dollars
-						CNT_DOLLARS += money.StackObjectsCount;
-						break;
-					case "569668774bdc2da2298b4568": // EUROS
-						CNT_EUROS += money.StackObjectsCount;
-						break;
-				}
-			}
-			NumberFormatInfo provider = new NumberFormatInfo
-			{
-				NumberGroupSeparator = " "
-			};
-			___roubles.text = CNT_ROUBLES.ToString("N0", provider);
-			___dollars.text = CNT_DOLLARS.ToString("N0", provider);
-			___euros.text = CNT_EUROS.ToString("N0", provider);
-			return false;
-		}
-		internal static IEnumerable<Item> GetAllItems(Item item) {
-			return item.GetAllItems(false);
-		}
-		internal static bool IsMoney(EFT.InventoryLogic.Item item)
-		{
-			return item.TemplateId == "5449016a4bdc2d6f028b456f" ||
-				item.TemplateId == "5696686a4bdc2da3298b456a" ||
-				item.TemplateId == "569668774bdc2da2298b4568";
-		}
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(EFT.UI.DisplayMoneyPanel).GetMethod(_MethodName, BindingFlags.Public | BindingFlags.Instance);
+        }
+        /* need to be patched using transpiler and edited a part where GetMoneySums is called then returned the one below ;)
+        EFT.ItemRequirementsPanel.CreateItemsViews(GClass1560, GClass1928[], Action<bool>) : void @06007242
+        EFT.UI.HealthTreatmentServiceView.method_3() : void @0600ACC1
+        EFT.UI.Insurance.InsurerParametersPanel.UpdateLabels(int, bool, bool) : void @0600BCFA
+        EFT.UI.QuestObjectiveView.method_0(GClass1448) : void @0600AB9D
+        EFT.UI.Ragfair.AddOfferWindow.method_3() : void @0600C030
+        EFT.UI.Ragfair.RenewOfferWindow.method_2() : void @0600C17E
+        EFT.UI.RepairerParametersPanel.UpdateLabels() : void @0600A4BC
+        GClass1460.method_16(GClass1781, ConditionFindItem) : int @06007FF2
+        */
+        private static bool PatchPrefix(
+            IEnumerable<EFT.InventoryLogic.Item> inventoryItems,
+            EFT.UI.DisplayMoneyPanel __instance,
+            CustomTextMeshProUGUI ___roubles,
+            CustomTextMeshProUGUI ___euros,
+            CustomTextMeshProUGUI ___dollars)
+        {
+            __instance.ShowGameObject(false);
+            var items = inventoryItems.SelectMany(new Func<Item, IEnumerable<Item>>(GetAllItems));
+            var OnlyMoney = items.Where(new Func<EFT.InventoryLogic.Item, bool>(IsMoney));
+            long CNT_ROUBLES = 0;
+            long CNT_DOLLARS = 0;
+            long CNT_EUROS = 0;
+            foreach (var money in OnlyMoney) {
+                switch (money.TemplateId) {
+                    case "5449016a4bdc2d6f028b456f": // Roubles
+                        CNT_ROUBLES += money.StackObjectsCount;
+                        break;
+                    case "5696686a4bdc2da3298b456a": // Dollars
+                        CNT_DOLLARS += money.StackObjectsCount;
+                        break;
+                    case "569668774bdc2da2298b4568": // EUROS
+                        CNT_EUROS += money.StackObjectsCount;
+                        break;
+                }
+            }
+            NumberFormatInfo provider = new NumberFormatInfo
+            {
+                NumberGroupSeparator = " "
+            };
+            ___roubles.text = CNT_ROUBLES.ToString("N0", provider);
+            ___dollars.text = CNT_DOLLARS.ToString("N0", provider);
+            ___euros.text = CNT_EUROS.ToString("N0", provider);
+            return false;
+        }
+        internal static IEnumerable<Item> GetAllItems(Item item) {
+            return item.GetAllItems(false);
+        }
+        internal static bool IsMoney(EFT.InventoryLogic.Item item)
+        {
+            return item.TemplateId == "5449016a4bdc2d6f028b456f" ||
+                item.TemplateId == "5696686a4bdc2da3298b456a" ||
+                item.TemplateId == "569668774bdc2da2298b4568";
+        }
 
 
-		// for transpiler patching
-		internal static Dictionary<ECurrencyType, long> GetMoneySums_Patched(IEnumerable<Item> items) {
-			Dictionary<ECurrencyType, long> MoneyDict = new Dictionary<ECurrencyType, long>() {
-				{ ECurrencyType.EUR, 0 },
-				{ ECurrencyType.RUB, 0 },
-				{ ECurrencyType.USD, 0 }
-			};
-			foreach (var money in items)
-			{
-				switch (money.TemplateId)
-				{
-					case "5449016a4bdc2d6f028b456f": // Roubles
-						MoneyDict[ECurrencyType.RUB] += money.StackObjectsCount;
-						break;
-					case "5696686a4bdc2da3298b456a": // Dollars
-						MoneyDict[ECurrencyType.USD] += money.StackObjectsCount;
-						break;
-					case "569668774bdc2da2298b4568": // EUROS
-						MoneyDict[ECurrencyType.EUR] += money.StackObjectsCount;
-						break;
-				}
-			}
-			return MoneyDict;
-		}
-	}
+        // for transpiler patching
+        internal static Dictionary<ECurrencyType, long> GetMoneySums_Patched(IEnumerable<Item> items) {
+            Dictionary<ECurrencyType, long> MoneyDict = new Dictionary<ECurrencyType, long>() {
+                { ECurrencyType.EUR, 0 },
+                { ECurrencyType.RUB, 0 },
+                { ECurrencyType.USD, 0 }
+            };
+            foreach (var money in items)
+            {
+                switch (money.TemplateId)
+                {
+                    case "5449016a4bdc2d6f028b456f": // Roubles
+                        MoneyDict[ECurrencyType.RUB] += money.StackObjectsCount;
+                        break;
+                    case "5696686a4bdc2da3298b456a": // Dollars
+                        MoneyDict[ECurrencyType.USD] += money.StackObjectsCount;
+                        break;
+                    case "569668774bdc2da2298b4568": // EUROS
+                        MoneyDict[ECurrencyType.EUR] += money.StackObjectsCount;
+                        break;
+                }
+            }
+            return MoneyDict;
+        }
+    }
 }
--- JET/Utilities/Patching/PatcherUtil.cs
+++ JET/Utilities/Patching/PatcherUtil.cs
@@ -5,61 +5,61 @@ using UnityEngine;
 
 namespace JET.Utilities.Patching
 {
-	public static class PatcherUtil
-	{
-		private static Harmony harmony;
+    public static class PatcherUtil
+    {
+        private static Harmony harmony;
 
-		static PatcherUtil()
-		{
-			harmony = new Harmony("com.[JET]");
-		}
-
-		public static MethodInfo GetOriginalMethod<T>(string methodName)
-		{
-			return AccessTools.Method(typeof(T), methodName);
-		}
-		
-		public static void PatchPrefix<T>() where T : AbstractPatch, new()
+        static PatcherUtil()
         {
-			harmony.Patch(new T().TargetMethod(), prefix: new HarmonyMethod(typeof(T).GetMethod("Prefix")));
-			Debug.Log("[JET]: Applied prefix patch " + typeof(T).Name);
-		}
+            harmony = new Harmony("com.[JET]");
+        }
 
-		public static void PatchPostfix<T>() where T : AbstractPatch, new()
+        public static MethodInfo GetOriginalMethod<T>(string methodName)
         {
-			harmony.Patch(new T().TargetMethod(), postfix: new HarmonyMethod(typeof(T).GetMethod("Postfix")));
-			Debug.Log("[JET]: Applied postfix patch " + typeof(T).Name);
-		}
+            return AccessTools.Method(typeof(T), methodName);
+        }
+        
+        public static void PatchPrefix<T>() where T : AbstractPatch, new()
+        {
+            harmony.Patch(new T().TargetMethod(), prefix: new HarmonyMethod(typeof(T).GetMethod("Prefix")));
+            Debug.Log("[JET]: Applied prefix patch " + typeof(T).Name);
+        }
 
-		public static void Patch<T>() where T : GenericPatch<T>, new()
-		{
-			try
-			{
-				var patch = new T();
-				if (patch.TargetMethod == null)
-					throw new InvalidOperationException("TargetMethod is null");
+        public static void PatchPostfix<T>() where T : AbstractPatch, new()
+        {
+            harmony.Patch(new T().TargetMethod(), postfix: new HarmonyMethod(typeof(T).GetMethod("Postfix")));
+            Debug.Log("[JET]: Applied postfix patch " + typeof(T).Name);
+        }
 
-				harmony.Patch(patch.TargetMethod,
-							  prefix: patch.Prefix.ToHarmonyMethod(),
-							  postfix: patch.Postfix.ToHarmonyMethod(),
-							  transpiler: patch.Transpiler.ToHarmonyMethod(),
-							  finalizer: patch.Finalizer.ToHarmonyMethod());
-				Debug.LogError("[JET]: Applied patch " + typeof(T).Name);
-			}
-			catch (Exception ex)
-			{
-				Debug.LogError($"[JET]: Error in patch {typeof(T).Name}{Environment.NewLine}{ex}");
-			}
-		}
-	}
+        public static void Patch<T>() where T : GenericPatch<T>, new()
+        {
+            try
+            {
+                var patch = new T();
+                if (patch.TargetMethod == null)
+                    throw new InvalidOperationException("TargetMethod is null");
 
-	static class Extensions
-	{
-		public static HarmonyMethod ToHarmonyMethod(this MethodInfo methodInfo)
-		{
-			return methodInfo != null 
-				? new HarmonyMethod(methodInfo)
-				: null;
-		}
-	}
+                harmony.Patch(patch.TargetMethod,
+                              prefix: patch.Prefix.ToHarmonyMethod(),
+                              postfix: patch.Postfix.ToHarmonyMethod(),
+                              transpiler: patch.Transpiler.ToHarmonyMethod(),
+                              finalizer: patch.Finalizer.ToHarmonyMethod());
+                Debug.LogError("[JET]: Applied patch " + typeof(T).Name);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"[JET]: Error in patch {typeof(T).Name}{Environment.NewLine}{ex}");
+            }
+        }
+    }
+
+    static class Extensions
+    {
+        public static HarmonyMethod ToHarmonyMethod(this MethodInfo methodInfo)
+        {
+            return methodInfo != null 
+                ? new HarmonyMethod(methodInfo)
+                : null;
+        }
+    }
 }
