cfa9052a16f9ca8a9448278b52e3ed8425e991c4
aclengen <aclengen@gmail.com>
2021-11-19 07:39:01 -0500

    Create 15426.cs

---
+++ GClass List/15426.cs
@@ -0,0 +1,27 @@
+Assembly Game Version: 0.12.11.7.15426
+Current => New
+
+using MainMenuController = GClass1474;
+using IHealthController = GInterface189;
+using StDamage = GStruct242;
+using IEffect = GInterface148;
+using ISession = GInterface112;
+using IBundleLock = GInterface276;
+using BotDifficultyHandler = GClass506;
+using WaveInfo = GClass1205;
+using BotsPresets = GClass565;
+using BotData = GInterface18;
+using PoolManager = GClass1454;
+using JobPriority = GClass2493;
+using Requirement = GClass1578;
+using HideoutInstance = GClass1554;
+using ClientMetrics = GClass1678;
+using ISpawnPoints = GInterface241;
+using AmmoInfo = GClass2029;
+using Equipment = GClass2013;
+using DamageInfo = GStruct242;
+using UI_Button = EFT.UI.DefaultUIButton;
+using MenuController = GClass1474;
+using WeatherSettings = GStruct93;
+using BotsSettings = GStruct234;
+using WavesSettings = GStruct94;
